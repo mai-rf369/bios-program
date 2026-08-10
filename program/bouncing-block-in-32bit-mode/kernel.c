0e8a8d3e6d1980aeb89589cc3e10bed09974daa4
Tayonism <116451284+mai-rf369@users.noreply.github.com>
2026-08-10 18:10:31 +0900

    Update kernel.c

--- program/bouncing-block-in-32bit-mode/kernel.c
+++ program/bouncing-block-in-32bit-mode/kernel.c
@@ -4,7 +4,9 @@
 #define SCREEN_HEIGHT 200
 
 static inline unsigned char inb(unsigned short port);
+static inline void outb(unsigned short port, unsigned char data);
 void wait_vsync(void);
+void delay_ms(unsigned int ms);
 
 void main(void)
 {
@@ -48,36 +50,7 @@ void main(void)
 		vram[y * SCREEN_WIDTH + x]	=	dot_color;
 		
 		// Weight for Speed Adjustment
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
-		wait_vsync();
+		delay_ms(16);
 	}
 }
 
@@ -89,6 +62,12 @@ static inline unsigned char inb(unsigned short port)
 	__asm__ volatile ("inb %1, %0" : "=a"(result) : "Nd"(port));
 	
 	return result;
+}
+
+// Write 1 Byte to IO Port
+static inline void outb(unsigned short port, unsigned char data)
+{
+	__asm__ volatile ("outb %0, %1" : : "a"(data), "Nd"(port));
 }
 
 // Wait VSYNC
@@ -104,4 +83,46 @@ void wait_vsync(void)
 	// Wait VSYNC Start
 	while (!(inb(0x03DA) & 0x08))
 	{}
+}
+
+// Read Current PIT Channel 0 Count
+unsigned short read_pit(void)
+{
+	unsigned short count;
+	
+	// Send Latch Command (0x00) to PIT Command Port (0x43)
+	outb(0x43, 0x00);
+	
+	count	=	inb(0x40);			// Read Low Byte
+	count	=	count | (inb(0x40) << 8);	// Read High Byte
+	
+	return count;
+}
+
+// Wait for specified MilliSeconds
+void delay_ms(unsigned int ms)
+{
+	// PIT runs exactly at 1,193,182Hz. (1ms = 1,193 ticks)
+	unsigned int target_ticks	=	ms * 1193;
+	unsigned int elapsed_ticks	=	0;
+	
+	unsigned short current_tick	=	read_pit();
+	unsigned short prev_tick	=	current_tick();
+	
+	while (elapsed_ticks < target_ticks)
+	{
+		current_tick	=	read_pit();
+		
+		// PIT is a Decrementing Counter
+		if (prev_tick >= current_tick)
+		{
+			elapsed_ticks	=	elapsed_ticks + (prev_tick - current_tick);
+		}
+		else
+		{
+			elapsed_ticks	=	elapsed_ticks + (prev_tick + (65536 - current_tick));
+		}
+		
+		prev_tick	=	current_tick;
+	}
 }
