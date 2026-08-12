17b966001e2d294f6edddd5ab047566e86b3bb33
Tayonism <116451284+mai-rf369@users.noreply.github.com>
2026-08-12 11:30:53 +0900

    Create keyboard.c

---
+++ program/acpi-shutdown-in-32bit-mode/keyboard.c
@@ -0,0 +1,30 @@
+#include "keyboard.h"
+#include "hardware.h"
+
+void process_keyboard(int *key_state)
+{
+	// Read Status Register in Keyboard Controller
+	while (inb(0x64) & 0x01)
+	{
+		// Read Scan Code from Data Port (0x60)
+		unsigned char scan	=	inb(0x60);
+		
+		// Skip Extended Key Prefix
+		if (scan == 0xE0)
+		{
+			continue;
+		}
+		
+		if (scan & 0x80)
+		{
+			// Release
+			key_state[scan & 0x7F]	=	0;
+		}
+		else
+		{
+			// Press
+			key_state[scan]		=	1;
+		}
+	}
+}
+
